b26c1c4cb7b59b4188f636e2c2de30ea116a7763
«lenalog13» <lenaloginova2000@mail.ru>
2024-10-20 19:25:53 +0300

    navbar v2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './Components/Navbar.css';
-
+import React from 'react';
+import './Navbar.css';
 
 export default function Navbar() {
   return (
     <nav className="navbar">
+      <div className="navbar-logo">
+        <h2>Музей истории <br /> детского движения</h2>
+      </div>
+  
       <div className="navbar-left">
-        <div className="navbar-logo">
-          <h2>Музей истории детского движения</h2>
-        </div>
         <ul className="navbar-links">
-          <li><Link to="#" onClick={(e) => e.preventDefault()}>Все экспонаты</Link></li>
-          <li><Link to="#" onClick={(e) => e.preventDefault()}>Выставки</Link></li>
+          <li>
+            <a href="#" onClick={(e) => e.preventDefault()}>Все экспонаты</a>
+          </li>
+          <li>
+            <a href="#" onClick={(e) => e.preventDefault()}>Выставки</a>
+          </li>
         </ul>
       </div>
+
       <div className="navbar-right">
-        <Link to="#" onClick={(e) => e.preventDefault()}>Войти</Link>
+        <ul className="navbar-links">
+          <li>
+            <a href="#" onClick={(e) => e.preventDefault()}>Войти</a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
 }
+
