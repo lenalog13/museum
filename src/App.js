83dc3a48e62d3593c18393aa915a4fa4944c2acd
«lenalog13» <lenaloginova2000@mail.ru>
2024-10-22 21:22:00 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Navbar from "./Components/Navbar";
-import Home from "./Pages/Home";
+import React from 'react'
+import Navbar from "./Components/Navbar"
+import Home from "./Pages/Home"
+import Header from "./Components/Header"
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Header></Header>
       <Home></Home>
     </>
   );
---
+++ src/Components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+
+    let catalog = [
+        {
+            exhibition: 'выставка 1',
+            showcases: [ 'витрина 1', 'витрина 2' ]
+        },
+        {
+            exhibition: 'выставка 2',
+            showcases: [ 'витрина 1', 'витрина 2' ]
+        }
+    ];
+
+    // Функция для определения правильного окончания слова "выставка"
+    const getExhibitionWord = (count) => {
+        if (count % 10 === 1 && count % 100 !== 11) {
+            return 'выставка';
+        } else if ((count % 10 >= 2 && count % 10 <= 4) && (count % 100 < 10 || count % 100 >= 20)) {
+            return 'выставки';
+        } else {
+            return 'выставок';
+        }
+    };
+
+    const exhibitionCount = catalog.length;
+    const exhibitionWord = getExhibitionWord(exhibitionCount);
+
+    return (
+        <nav className="header">
+            <div className="header-left">
+                Выставки </div>
+
+            <div className="header-right">
+                Найдено {exhibitionCount} {exhibitionWord}
+            </div>
+        </nav>
+    );
+}
