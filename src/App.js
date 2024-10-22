c1057980f6fc1a12f746bd2b909c3b9f0d899342
«lenalog13» <lenaloginova2000@mail.ru>
2024-10-22 23:23:38 +0300

    passing parameters

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ import Home from "./Pages/Home"
 
 
 function App() {
+
+  const catalog = [
+    {
+        id: 0,
+        exhibition: 'выставка 1',
+        showcases: ['витрина 1', 'витрина 2']
+    },
+    {
+        id: 1,
+        exhibition: 'выставка 2',
+        showcases: ['витрина 1', 'витрина 2', 'витрина 3']
+    },
+    {
+      id: 2,
+      exhibition: 'выставка 3',
+      showcases: ['витрина 1', 'витрина 2', 'витрина 3']
+  }
+];
+
   return (
     <>
       <Navbar></Navbar>
-      <Header></Header>
-      <Home></Home>
+      <Header catalog={catalog}></Header>
+      <Home catalog={catalog}></Home>
     </>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import './Header.css';
 
-export default function Header() {
+const Header = ({ catalog }) => {
 
-    let catalog = [
-        {
-            id: 0,
-            exhibition: 'выставка 1',
-            showcases: [ 'витрина 1', 'витрина 2' ]
-        },
-        {
-            id: 1,
-            exhibition: 'выставка 2',
-            showcases: [ 'витрина 1', 'витрина 2', 'витрина 3' ]
-        }
-    ];
-
-    // Функция для определения правильного окончания слова "выставка"
-    const getExhibitionWord = (count) => {
+     // Функция для определения правильного окончания слова "выставка"
+     const getExhibitionWord = (count) => {
         if (count % 10 === 1 && count % 100 !== 11) {
             return 'выставка';
         } else if ((count % 10 >= 2 && count % 10 <= 4) && (count % 100 < 10 || count % 100 >= 20)) {
@@ -30,6 +17,7 @@ export default function Header() {
     const exhibitionCount = catalog.length;
     const exhibitionWord = getExhibitionWord(exhibitionCount);
 
+
     return (
         <nav className="header">
             <div className="header-left">
@@ -40,4 +28,6 @@ export default function Header() {
             </div>
         </nav>
     );
-}
+};
+
+export default Header;
