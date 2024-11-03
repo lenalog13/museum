2b6b26867ada9a882ed4f15ff363db3f32eb0210
«lenalog13» <lenaloginova2000@mail.ru>
2024-11-03 16:01:35 +0300

    correct sidebar

--- src/Pages/Racks.jsx
+++ src/Pages/Racks.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './List.css';
 import Header from '../Components/Header'; 
@@ -6,17 +6,13 @@ import Sidebar from '../Components/Sidebar';
 
 export default function Racks() {
 
-  const [selectedItem, setSelectedItem] = useState(null);
-
-
   const items = [
     { id: 0, title: 'Стеллажи', racks: [{ id: 0, racksName: 'стеллаж 1' }, { id: 1, racksName: 'стеллаж 2' }, { id: 2, racksName: 'стеллаж 3' }] },
     { id: 1, title: 'Шкафы', racks: [{ id: 0, racksName: 'шкаф 1' }, { id: 1, racksName: 'шкаф 2' }, { id: 2, racksName: 'шкаф 3' }] },
     { id: 2, title: 'Сейфы', racks: [{ id: 0, racksName: 'сейф 1' }, { id: 1, racksName: 'сейф 2' }] }
   ];
 
-
-  useEffect(() => {setSelectedItem(items[0]); }, []);
+  const [selectedItem, setSelectedItem] = useState(items[0]);
 
   return (
     <div>
