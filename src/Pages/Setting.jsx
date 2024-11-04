9e5e52312683898a7b0227f57acb83873424f3b2
«lenalog13» <lenaloginova2000@mail.ru>
2024-11-04 19:06:32 +0300

    correct setting v1.2

--- src/Pages/Setting.jsx
+++ src/Pages/Setting.jsx
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './List.css';
 import './Setting.css';
 import Header from '../Components/Header'; 
 
 export default function Setting() {
 
-  const catalog = {
-    title: 'Пользователи',
-    users: [
-      { id: 0, name: 'Чернышов А.В.', rights: 'admin', login: 'sacha', password: '12345' },
-      { id: 1, name: 'Модератор1', rights: 'moderator', login: 'moderator1', password: 'moderator1' },
-      { id: 2, name: 'Модератор2', rights: 'moderator', login: 'moderator2', password: 'moderator2' },
-    ]
-  };
+    const catalog = {
+        title: 'Пользователи',
+        users: [
+          { id: 0, name: 'Чернышов А.В.', rights: 'admin', login: 'sacha', password: '12345' },
+          { id: 1, name: 'Модератор 1', rights: 'moderator', login: 'moderator1', password: 'moderator1' },
+          { id: 2, name: 'Модератор 2', rights: 'moderator', login: 'moderator2', password: 'moderator2' },
+        ]
+    };
 
+      const [modalVisible, setModalVisible] = useState(false);
+  
+      const [editingUserId, setEditingUserId] = useState(null); // Состояние для редактируемого пользователя
+  
+      const [newUser, setNewUser] = useState({ name: '', rights: 'moderator', login: '', password: '' })
+
+      const handleEditUser  = (user) => {
+          setNewUser (user); // Заполнение формы данными пользователя
+          setEditingUserId(user.id); // Установка ID редактируемого пользователя
+          setModalVisible(true); // Показать модальное окно
+      };
+  
+      const handleCancel = () => {
+          resetForm(); // Сброс формы
+      };
+  
+      const resetForm = () => {
+          setNewUser ({ name: '', rights: 'moderator', login: '', password: '' }); // Сброс формы
+          setEditingUserId(null); // Сброс ID редактируемого пользователя
+          setModalVisible(false); // Скрыть модальное окно
+      };
+  
 
   return (
     <div>
       <Header title={catalog.title} 
         count={null} />
-        <button className="adding-button">
+        <button className="adding-button" onClick={() => setModalVisible(true)}>
             + Добавить пользователя
         </button>
+
       <div className='classList'>
         <ul>
         {catalog.users.map((item) => (
@@ -29,13 +52,58 @@ export default function Setting() {
               <div>
                 {item.name} - {item.rights}
               </div>
-              <button className="setting-button">
+              <button className="setting-button" onClick={() => handleEditUser (item)}f >
                 Изменить
               </button>
             </li>
           ))}
         </ul>
       </div>
+
+      {modalVisible && (
+            <div className="modal-overlay">
+                <div className="modal">
+                    <h3>{editingUserId !== null ?
+                     'Редактировать пользователя' : 'Добавить пользователя'}
+                    </h3>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Имя"
+                        value={newUser.name}
+                    />
+                    <div className="rights-container">
+                        <label htmlFor="rights">Права:</label>
+                        <select name="rights" value={newUser.rights} id="rights">
+                            <option value="moderator">Модератор</option>
+                            <option value="admin">Админ</option>
+                        </select>
+                    </div>
+                    <input
+                        type="text"
+                        name="login"
+                        placeholder="Логин"
+                        value={newUser.login}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Пароль"
+                        value={newUser.password}
+                    />
+                    <div className="modal-buttons">
+                        <button className="cancel-button" onClick={handleCancel}>Отменить</button>
+                        {editingUserId !== null && (
+                                <button className="delete-button" onClick={handleCancel}>
+                                    Удалить
+                                </button>
+                        )}
+                        <button className="save-button" onClick={handleCancel}>Сохранить</button>
+                    </div>
+                </div>
+            </div>
+        )}
+
     </div>
   );
 }
