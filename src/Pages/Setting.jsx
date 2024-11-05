a870b21f2058eccd077f0b51be2d3d340a048951
«lenalog13» <lenaloginova2000@mail.ru>
2024-11-05 13:32:37 +0300

    correct setting

--- src/Pages/Setting.jsx
+++ src/Pages/Setting.jsx
@@ -14,97 +14,95 @@ export default function Setting() {
         ]
     };
 
-    const [modalVisible, setModalVisible] = useState(false);
-    const [editingUserId, setEditingUserId] = useState(null);
-    const [newUser, setNewUser] = useState({ name: '', rights: 'moderator', login: '', password: '' });
+      const [modalVisible, setModalVisible] = useState(false);
+  
+      const [editingUserId, setEditingUserId] = useState(null);
+  
+      const [newUser, setNewUser] = useState({ name: '', rights: 'moderator', login: '', password: '' })
 
-    const handleEditUser = (user) => {
-        setNewUser(user);
-        setEditingUserId(user.id);
-        setModalVisible(true);
-    };
+      const handleEditUser  = (user) => {
+          setNewUser(user);
+          setEditingUserId(user.id);
+          setModalVisible(true);
+      };
+  
+      const handleCancel = () => {
+          resetForm();
+      };
+  
+      const resetForm = () => {
+          setNewUser ({ name: '', rights: 'moderator', login: '', password: '' });
+          setEditingUserId(null);
+          setModalVisible(false);
+      };
+  
 
-    const handleCancel = () => {
-        resetForm();
-    };
+  return (
+    <div>
+      <Header title={catalog.title} 
+        count={null} />
+        <button className="adding-button" onClick={() => setModalVisible(true)}>
+            + Добавить пользователя
+        </button>
 
-    const resetForm = () => {
-        setNewUser({ name: '', rights: 'moderator', login: '', password: '' });
-        setEditingUserId(null);
-        setModalVisible(false);
-    };
+      <div className='classList'>
+        <ul>
+        {catalog.users.map((item) => (
+            <li key={item.id} className="list-item">
+              <div>
+                {item.name} - {item.rights}
+              </div>
+              <button className="setting-button" onClick={() => handleEditUser (item)}f >
+                Изменить
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setNewUser(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
-
-    return (
-        <div>
-            <Header title={catalog.title} count={null} />
-            <button className="adding-button" onClick={() => setModalVisible(true)}>
-                + Добавить пользователя
-            </button>
-
-            <div className='classList'>
-                <ul>
-                    {catalog.users.map((item) => (
-                        <li key={item.id} className="list-item">
-                            <div>
-                                {item.name} - {item.rights}
-                            </div>
-                            <button className="setting-button" onClick={() => handleEditUser(item)}>
-                                Изменить
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-
-            {modalVisible && (
-                <div className="modal-overlay">
-                    <div className="modal">
-                        <h3>{editingUserId !== null ? 'Редактировать пользователя' : 'Добавить пользователя'}</h3>
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Имя"
-                            value={newUser.name}
-                            onChange={handleInputChange}
-                        />
-                        <div className="rights-container">
-                            <select name="rights" value={newUser.rights} id="rights" onChange={handleInputChange}>
-                                <option value="moderator">Модератор</option>
-                                <option value="admin">Админ</option>
-                            </select>
-                        </div>
-                        <input
-                            type="text"
-                            name="login"
-                            placeholder="Логин"
-                            value={newUser.login}
-                            onChange={handleInputChange}
-                        />
-                        <input
-                            type="password"
-                            name="password"
-                            placeholder="Пароль"
-                            value={newUser.password}
-                            onChange={handleInputChange}
-                        />
-                        <div className="modal-buttons">
-                            {editingUserId !== null && (
-                                <button className="delete-button" onClick={handleCancel}>Удалить</button>
-                            )}
-                            <button className="cancel-button" onClick={handleCancel}>Назад</button>
-                            <button className="save-button" onClick={handleCancel}>Сохранить</button>
-                        </div>
+      {modalVisible && (
+            <div className="modal-overlay">
+                <div className="modal">
+                    <h3>{editingUserId !== null ?
+                     'Редактировать пользователя' : 'Добавить пользователя'}
+                    </h3>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Имя"
+                        value={newUser.name}
+                    />
+                    <div className="rights-container">
+                        <select name="rights" value={newUser.rights} id="rights">
+                            <option value="moderator">Модератор</option>
+                            <option value="admin">Админ</option>
+                        </select>
+                    </div>
+                    <input
+                        type="text"
+                        name="login"
+                        placeholder="Логин"
+                        value={newUser.login}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Пароль"
+                        value={newUser.password}
+                    />
+                    <div className="modal-buttons">
+                        {editingUserId !== null && (
+                                <button className="delete-button" onClick={handleCancel}>
+                                    Удалить
+                                </button>
+                        )}
+                        <button className="cancel-button" onClick={handleCancel}>Отменить</button>
+                        <button className="save-button" onClick={handleCancel}>Сохранить</button>
                     </div>
                 </div>
-            )}
-        </div>
-    );
+            </div>
+        )}
+
+    </div>
+  );
 }
