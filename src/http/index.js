03577b56e915d46657477336cbeb4ba336266634
«lenalog13» <lenaloginova2000@mail.ru>
2024-12-08 20:38:46 +0300

    correct api url

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = `http://localhost:8080/api/v1`
+export const API_URL = `http://localhost:8081/api/v1`
 
 const $api = axios.create({
     withCredentials: true,
